66b7be525c1afdb0b7d328d95e8983834e648c89
tiensyle <ltsy2812@gmail.com>
2026-09-03 18:10:37 +0700

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Google Firebase configuration for Boss Timeline Pro
+window.BOSS_TIMELINE_FIREBASE = {
+  apiKey: "AIzaSyCfVwcvXqDeurizrqEbHmGopNIPHFa-D7Q",
+  authDomain: "time-boss-chill.firebaseapp.com",
+  databaseURL: "https://time-boss-chill-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "time-boss-chill",
+  storageBucket: "time-boss-chill.firebasestorage.app",
+  messagingSenderId: "570269335733",
+  appId: "1:570269335733:web:db918759f03c6c1f13fefc",
+  measurementId: "G-Z6D8SJB48X"
+};
